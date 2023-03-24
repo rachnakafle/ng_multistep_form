2861c7483a31a5da08e28dc2d372ed5eef0c8bd9
rachnakafle <rachanakafle99@gmail.com>
2023-03-24 14:36:41 +0545

    Multistep form created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'multistep_form';
+  steps: number = 1;
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.steps);
+  }
+
+  previous(){
+    this.steps = this.steps-1;
+  }
+
+  OnSubmit() {
+    this.steps = this.steps + 1;
+    console.log(this.steps);
+    
+  }
 }
