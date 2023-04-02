ec483afde1dfdbc93fd4d806e36d20cbbb015595
Rachna Kafle <rachanakafle99@gmail.com>
2023-04-02 23:14:25 +0545

    Minor changes

--- src/app/form-container/form-container.component.ts
+++ src/app/form-container/form-container.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { BehaviorSubject, Observable } from 'rxjs';
-type Step = 'personalInfo' | 'loginInfo';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+// import { BehaviorSubject, Observable } from 'rxjs';
+// type Step = 'personalInfo' | 'loginInfo';
 
 @Component({
   selector: 'form-container',
@@ -13,27 +18,30 @@ export class FormContainerComponent implements OnInit {
 
   multiStep = new FormGroup({
     userDetails: new FormGroup({
-      fname: new FormControl(''),
-      uname: new FormControl(''),
+      fname: new FormControl('', [Validators.required]),
+      uname: new FormControl('', [Validators.required]),
     }),
     contactDetails: new FormGroup({
-      email: new FormControl(''),
-      mobile: new FormControl(''),
-      address: new FormControl(''),
+      email: new FormControl('', [Validators.required]),
+      mobile: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
     }),
     personalDetails: new FormGroup({
-      gender: new FormControl(''),
-      education: new FormControl(''),
+      gender: new FormControl('', [Validators.required]),
+      education: new FormControl('', [Validators.required]),
     }),
   });
 
-  constructor(private _fb: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    // this.userForm = this._fb.group({
-    //   personalInfo: null,
-    //   loginInfo: null,
-    // });
+  ngOnInit(): void {}
+
+  get fname() {
+    return this.multiStep.get('userDetails.fname');
+  }
+
+  get uname() {
+    return this.multiStep.get('userDetails.uname');
   }
 
   previous() {
@@ -41,10 +49,15 @@ export class FormContainerComponent implements OnInit {
   }
 
   OnSubmit() {
-    this.steps = this.steps + 1;
-    console.log(this.steps);
-    if (this.steps == 4) {
-      // this._router.navigate(['thank-you']);
+    // console.log(this.steps);
+    // if (this.steps == 4) {
+    // this._router.navigate(['thank-you']);
+    console.log(this.multiStep.valid);
+
+    if (this.multiStep.valid) {
+      
+      this.steps = this.steps + 1;
     }
   }
+  // }
 }
